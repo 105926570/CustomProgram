791c66b747cf1d3dcec2f7dba28dd8fc965b2a7b
Thomas Federico <105926570@student.swin.edu.au>
2025-10-18 19:46:37 +1100

    Added  properties to user

--- PayrollSystem/Employee.cs
+++ PayrollSystem/Employee.cs
@@ -42,6 +42,18 @@ namespace PayrollSystem
                 return s;
             }
         }
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
     }
 }
 
