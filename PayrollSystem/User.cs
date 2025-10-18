fcb9a9db36f52aff7364e52c9ac619476e4e3188
Thomas Federico <105926570@student.swin.edu.au>
2025-10-18 19:32:35 +1100

    Added Variables and Empty Constructors

--- PayrollSystem/User.cs
+++ PayrollSystem/User.cs
@@ -8,5 +8,20 @@ namespace PayrollSystem
 {
     internal class User
     {
+        string Username, Password;
+        int UserID;
+
+        public User()
+        {
+        
+        }
+        public User(int userID, string username, string password)
+        {
+        
+        }
+        public User(int userID)
+        {
+        
+        }
     }
 }
