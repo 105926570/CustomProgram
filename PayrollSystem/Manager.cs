85b6140948abde0d09486926d36774ce23535440
Thomas Federico <105926570@student.swin.edu.au>
2025-10-18 19:27:48 +1100

    removed test code

--- PayrollSystem/Manager.cs
+++ PayrollSystem/Manager.cs
@@ -12,30 +12,12 @@ namespace PayrollSystem
 
         //Default constructor / Test constructor
         public Manager() : base() 
-        {
-            //test employees
-            Employee testEmployee1 = new Employee("Micheal", "Stevens");
-            Employee testEmployee2 = new Employee("Your", "Mum");
-            Employee testEmployee3 = new Employee("Lorenzo", "Federico");
-            //adding test employees to managed employees list
-            managedEmployees.Add(testEmployee1);
-            managedEmployees.Add(testEmployee2);
-            managedEmployees.Add(testEmployee3);
-        }
+        { }
 
         //All values provided except employees
         public Manager(int userID, string username, string password, string firstName, string lastName)
         : base(userID, username, password, firstName, lastName)
-        {
-            //test employees
-            Employee testEmployee1 = new Employee("Micheal", "Stevens");
-            Employee testEmployee2 = new Employee("Your", "Mum");
-            Employee testEmployee3 = new Employee("Lorenzo", "Federico");
-            //adding test employees to managed employees list
-            managedEmployees.Add(testEmployee1);
-            managedEmployees.Add(testEmployee2);
-            managedEmployees.Add(testEmployee3);
-        }
+        { }
 
         // All values provided
         public Manager(List<Employee> employees, int userID, string username, string password, string firstName, string lastName)
