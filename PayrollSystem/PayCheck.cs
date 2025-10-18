cfcdf81b04ef8c4de885be22ee46bcd61db9f143
Thomas Federico <105926570@student.swin.edu.au>
2025-10-18 21:45:19 +1100

    Implemented elements of PayHistory

--- PayrollSystem/PayCheck.cs
+++ PayrollSystem/PayCheck.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PayrollSystem
 {
-    internal class PayCheck
+    public class PayCheck
     {
     }
 }
--- PayrollSystem/PayHistory.cs
+++ PayrollSystem/PayHistory.cs
@@ -8,5 +8,17 @@ namespace PayrollSystem
 {
     public class PayHistory
     {
+        public List<PayCheck> payChecks = new List<PayCheck> { } ;
+        public PayHistory() { }
+        public PayHistory(List<PayCheck> payChecks) //Generate the pay history given there is allready a list of paychecks (likely wont be used)
+        {
+            this.PayChecks = payChecks;
+        }
+
+        public List<PayCheck> PayChecks
+        {
+            get { return payChecks; }
+            set { payChecks = value; }
+        }
     }
 }
