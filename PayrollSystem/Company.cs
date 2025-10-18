fcb476c4288af53cbb18f496feb5769022716a27
Thomas Federico <105926570@student.swin.edu.au>
2025-10-18 20:17:58 +1100

    Added Simple variables to company class

--- PayrollSystem/Company.cs
+++ PayrollSystem/Company.cs
@@ -8,5 +8,10 @@ namespace PayrollSystem
 {
     internal class Company
     {
+        public string companyName;
+        //public List<Department> departments;
+        public List<Employee> employees = new List<Employee>();
+        //public Payroll payroll;
+        //public Schedule schedule;
     }
 }
