eead56f07dad900b9955006e3bcfd1e9af3776de
Thomas Federico <105926570@student.swin.edu.au>
2025-10-18 20:42:52 +1100

    added constructors to department class

--- PayrollSystem/Department.cs
+++ PayrollSystem/Department.cs
@@ -13,5 +13,22 @@ namespace PayrollSystem
         private List<Employee> _employees = new List<Employee> { };
         private List<Manager> _managers = new List<Manager> { };
         private DateTime datetime = DateTime.Now;
+
+        public Department()
+        {
+            _departmentID = new Random(datetime.Millisecond).Next(99);
+            _departmentName = "Default Department";
+        }
+
+        public Department(string name)
+        {
+            _departmentName = name;
+        }
+
+        public Department(string name, int ID)
+        {
+            _departmentID = ID;
+            _departmentName = name;
+        }
     }
 }
