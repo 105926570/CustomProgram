7920f0eb8be3b6a85de6ab51d033c09dbbc7db96
Thomas Federico <105926570@student.swin.edu.au>
2025-10-18 20:42:36 +1100

    added variables to Department class

--- PayrollSystem/Department.cs
+++ PayrollSystem/Department.cs
@@ -8,5 +8,10 @@ namespace PayrollSystem
 {
     public class Department
     {
+        int _departmentID;
+        private string _departmentName;
+        private List<Employee> _employees = new List<Employee> { };
+        private List<Manager> _managers = new List<Manager> { };
+        private DateTime datetime = DateTime.Now;
     }
 }
