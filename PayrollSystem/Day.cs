88d5b51159b32f3d5f3047f07d05e79489169481
Thomas Federico <105926570@student.swin.edu.au>
2025-10-20 13:50:55 +1100

    created TimeFrameFromListIndex function in DayClass

--- PayrollSystem/Day.cs
+++ PayrollSystem/Day.cs
@@ -13,6 +13,17 @@ namespace PayrollSystem
 
         // Constructor to initialize the Day with empty TimeFrames
 
-
+        public TimeFrame TimeFrameFromListIndex(int index)
+        {
+            if (_timeFrames[index] == null)
+            {
+                Console.WriteLine($"Errod: No Time frame found at index {index.ToString()} ");
+                return null;
+            }
+            else
+            {
+                return _timeFrames[index];
+            }
+        }
     }
 }
