e83349462c5c7f821c21ed9cd29a489c7d04e80c
Thomas Federico <105926570@student.swin.edu.au>
2025-10-20 13:35:55 +1100

    created pronoun class

---
+++ PayrollSystem/Pronoun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSystem
+{
+    public class Pronoun
+    {
+        private int presetPronounType; //0 = they/them, 1 = he/him, 2 = she/her, 3 = custom
+    }
+}
