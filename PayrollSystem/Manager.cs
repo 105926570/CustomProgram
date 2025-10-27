d0bb604d4588d1c7a7e28d3ffe6adc22962a4e7b
Thomas Federico <105926570@student.swin.edu.au>
2025-10-27 19:33:46 +1100

    added privlege property to Manager aded privlege to manager constructors

--- PayrollSystem/Manager.cs
+++ PayrollSystem/Manager.cs
@@ -9,27 +9,38 @@ namespace PayrollSystem
     public class Manager : Employee
     {
         private List<Employee> _managedEmployees = new List<Employee> { };
+        private int _privilege; // Privilege level of a manager is level 1
 
         //Default constructor / Test constructor
         public Manager() : base() 
-        { }
+        {
+            _privilege = 1;
+        }
 
         //All values provided except employees
         public Manager(int employeeID, string username, string password, string firstName, string lastName)
         : base(employeeID, username, password, firstName, lastName)
-        { }
+        {
+            _privilege = 1;
+        }
 
         // All values provided
         public Manager(List<Employee> employees, int employeeID, string username, string password, string firstName, string lastName)
         : base(employeeID, username, password, firstName, lastName) 
         {
             _managedEmployees = employees;
+            _privilege = 1;
         }
 
         public List<Employee> ManagedEmployees
         {
             get { return _managedEmployees; }
             set { _managedEmployees = value; }
+        }
+
+        public int Privilege
+        {
+            get { return _privilege; }
         }
 
         public void DisplayManagedEmployees()
