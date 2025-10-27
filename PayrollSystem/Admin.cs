6097c7a531fc2e47a3a7fd64685a785a1439e96f
Thomas Federico <105926570@student.swin.edu.au>
2025-10-27 19:30:45 +1100

    Admin default constructor

--- PayrollSystem/Admin.cs
+++ PayrollSystem/Admin.cs
@@ -11,6 +11,11 @@ namespace PayrollSystem
     {
         private int _privliage;
 
+        public Admin() : base()
+        {
+            _privliage = 2; //Privliage of an admin is level 2
+        }
+
         public int Privliage
         {
             get { return _privliage; }
