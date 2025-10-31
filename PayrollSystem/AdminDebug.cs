b02645ee526bee2feb6b68ce252d60f94493f043
Thomas Federico <105926570@student.swin.edu.au>
2025-10-31 21:59:23 +1100

    created "pages"

--- PayrollSystem/AdminDebug.cs
+++ PayrollSystem/AdminDebug.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Reflection;
 using System.Windows.Forms;
 
@@ -6,45 +8,53 @@ namespace PayrollSystem
 {
     public partial class AdminDebug : Form
     {
-        private int globalTop = 20;     // vertical placement inside parent
-        private int globalLeft = 20;    // horizontal offset for columns
-        private int columnWidth = 325;  // width of each child group box + spacing
+        private int globalTop = 20;
+        private int globalLeft = 20;
+        private int columnWidth = 325;
+        private int pageWidth; // max width for one page
         private GroupBox parentGroupBox;
 
-        // Track maximum extents of child boxes
-        private int maxRight = 0;
-        private int maxBottom = 0;
+        private List<List<GroupBox>> pages = new List<List<GroupBox>>(); // pages of group boxes
+        private int currentPage = 0;
 
         public AdminDebug()
         {
             InitializeComponent();
-            this.Width = 1080;
-            this.Height = 1080;
+            this.Width = 500;
+            this.Height = 600;
+            pageWidth = this.ClientSize.Width - 40; // leave some margin
 
             // Create parent group box
             parentGroupBox = new GroupBox();
             parentGroupBox.Text = "create";
             parentGroupBox.Left = 20;
             parentGroupBox.Top = 20;
+            parentGroupBox.Width = pageWidth;
+            parentGroupBox.Height = this.ClientSize.Height - 80;
             //parentGroupBox.AutoScroll = true;
             this.Controls.Add(parentGroupBox);
 
-            // Generate child group boxes inside parent
+            // Generate child group boxes
             GenerateTextBoxesForClass(typeof(Department));
             GenerateTextBoxesForClass(typeof(Employee));
             GenerateTextBoxesForClass(typeof(Admin));
 
-            // Resize parent to fit content (with some padding)
-            parentGroupBox.Width = Math.Max(maxRight + 20, 300);
-            parentGroupBox.Height = Math.Max(maxBottom + 20, 300);
+            // Add button to navigate pages
+            Button nextPageBtn = new Button();
+            nextPageBtn.Text = "Next Page";
+            nextPageBtn.Left = 20;
+            nextPageBtn.Top = this.ClientSize.Height - 50;
+            nextPageBtn.Click += NextPageBtn_Click;
+            this.Controls.Add(nextPageBtn);
+
+            ShowPage(0); // show first page
         }
 
         private void GenerateTextBoxesForClass(Type targetClass)
         {
+            // Create a child group box
             GroupBox groupBox = new GroupBox();
             groupBox.Text = targetClass.Name;
-            groupBox.Left = globalLeft;
-            groupBox.Top = globalTop;
             groupBox.Width = 300;
 
             int innerTop = 20;
@@ -70,26 +80,54 @@ namespace PayrollSystem
 
             groupBox.Height = innerTop + 10;
 
-            // Check if adding next group would exceed parent height
-            int nextBottom = globalTop + groupBox.Height + 20;
-            if (nextBottom > this.ClientSize.Height - 40) // leave margin inside form
+            // Place group box on the current page, or start a new page
+            if (pages.Count == 0 || (globalLeft + groupBox.Width > pageWidth))
             {
-                // Move to new column
-                globalLeft += columnWidth;
+                // start a new page
+                pages.Add(new List<GroupBox>());
+                globalLeft = 20;
                 globalTop = 20;
-                groupBox.Left = globalLeft;
-                groupBox.Top = globalTop;
             }
 
-            // Add to parent
+            groupBox.Left = globalLeft;
+            groupBox.Top = globalTop;
+
             parentGroupBox.Controls.Add(groupBox);
+            pages[pages.Count - 1].Add(groupBox);
 
-            // Update max extents
-            maxRight = Math.Max(maxRight, groupBox.Left + groupBox.Width);
-            maxBottom = Math.Max(maxBottom, groupBox.Top + groupBox.Height);
+            // Update positions for next group box
+            globalLeft += groupBox.Width + 20;
+        }
 
-            // Move down for next group
-            globalTop += groupBox.Height + 20;
+        private void ShowPage(int pageIndex)
+        {
+            // Hide all group boxes first
+            foreach (var page in pages)
+            {
+                foreach (var gb in page)
+                {
+                    gb.Visible = false;
+                }
+            }
+
+            // Show only the requested page
+            if (pages.Count > 0)
+            {
+                foreach (var gb in pages[pageIndex])
+                {
+                    gb.Visible = true;
+                }
+            }
+
+            currentPage = pageIndex;
+        }
+
+        private void NextPageBtn_Click(object sender, EventArgs e)
+        {
+            if (pages.Count == 0) return;
+
+            int nextPage = (currentPage + 1) % pages.Count;
+            ShowPage(nextPage);
         }
     }
 }
