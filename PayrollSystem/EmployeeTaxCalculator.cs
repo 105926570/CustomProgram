a4da344ca8320aadc1b2f5b6a611ce4256f98f0e
Thomas Federico <105926570@student.swin.edu.au>
2025-10-31 20:14:57 +1100

    added tax bracket table

--- PayrollSystem/EmployeeTaxCalculator.cs
+++ PayrollSystem/EmployeeTaxCalculator.cs
@@ -48,7 +48,6 @@ namespace PayrollSystem
             {
                 return 4;
             }
-
         }
 
 
@@ -82,8 +81,6 @@ namespace PayrollSystem
 
 
 
-
-
         //properties:
         public double YearlyIncome
         {
@@ -107,3 +104,14 @@ namespace PayrollSystem
 //
 //  - make it so that when the yearly income is changed, the yearly tax is updated straight away, in case any references to the employee tax infos tax-to-pay are referred to.
 //
+//
+//Ensure that the following tax brackets are implemented in the payroll calculations:
+//
+///         Australian Tax Brackets for the 2023-2024 financial year
+/// [no.]   [yearly income]    [tax rate]	    [base tax plus]
+///     0   $0 – $18,200	       0%            ------------
+///     1   $18,201 – $45,000	  16%     	    16c for each $1 over $18,200
+///     2   $45,001 – $135,000	  30%	        $4,288 plus 30c for each $1 over $45,000
+///     3   $135,001 – $190,000	  37%	        $31,288 plus 37c for each $1 over $135,000
+///     4   $190,001 and over	  45%	        $51,638 plus 45c for each $1 over $190,000
+/// source: https://www.ato.gov.au/rates/individual-income-tax-rates/
