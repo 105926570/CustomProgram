45d44f9f8b237a4831dea964e1ab9a15c2d4dce1
Thomas Federico <h@THOMAS-PC>
2025-11-07 01:43:58 +1100

    Revert "added made it so the start date is allways a monday"
    This reverts commit 2d2eec7c6532799ff2376ee6e9cf002527fb49bb.

--- PayrollSystem/CallenderSystem/PayPeriod.cs
+++ PayrollSystem/CallenderSystem/PayPeriod.cs
@@ -14,14 +14,6 @@ namespace PayrollSystem.CallenderSystem
         public PayPeriod()
         {
             _startDate = DateTime.Now;
-            DayOfWeek currentDay = _startDate.DayOfWeek;
-
-            // Assuming Monday is the start of the week:
-            int daysSinceMonday = (int)currentDay - (int)DayOfWeek.Monday;
-            if (daysSinceMonday < 0)
-                daysSinceMonday += 7; // handle Sunday correctly
-
-            _startDate = _startDate.Date.AddDays(-daysSinceMonday);
             _endDate = _startDate.AddDays(7);
         }
         public PayPeriod(DateTime startDate)
