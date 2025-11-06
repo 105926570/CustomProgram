28b15ca895c83e00409759da3c11ea9443430d31
Thomas Federico <h@THOMAS-PC>
2025-11-07 01:33:22 +1100

    rebase

---
+++ PayrollSystem/CallenderSystem/PayPeriod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSystem.CallenderSystem
+{
+    internal class PayPeriod
+    {
+        private DateTime _startDate, _endDate;
+        private List<WorkedShifts> _workedShifts;
+
+        public PayPeriod(DateTime startDate)
+        {
+            _startDate = startDate;
+            _endDate = startDate.AddDays(7);
+        }
+        public PayPeriod(DateTime startDate, DateTime endDate)
+        {
+            _startDate = startDate;
+            _endDate = endDate;
+        }
+    }
+}
