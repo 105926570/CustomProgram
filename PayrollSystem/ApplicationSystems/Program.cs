3eba3729884592d7dc5f2e2c2ac3876ab9275705
Thomas Federico <105926570@student.swin.edu.au>
2025-11-08 16:47:15 +1100

    added documentaton to program.

--- PayrollSystem/ApplicationSystems/Program.cs
+++ PayrollSystem/ApplicationSystems/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PayrollSystem
@@ -15,12 +16,14 @@ namespace PayrollSystem
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginScreen());
         }
 
+        /// <summary>returns weather or not a user is active.</summary>
+        /// <returns>if the active user is null [no one is active], false is returned. otherwise it returns true.</returns>
         public static bool isLoggedIn
         {
             get
@@ -31,16 +34,33 @@ namespace PayrollSystem
             }
         }
 
+        /// <returns>
+        /// returns active employee.
+        /// OUTPUT CAN BE NULL IF NO EMPLOYEE IS LOGGED IN
+        /// </returns>
         public static Employee activeEmployee
-            { get { return _activeEmployee; } }
-            
+        {
+            get
+            {
+                return _activeEmployee;
+            }
+        }
+
+        /// <summary>changes active employee to whatever is given</summary>
+        /// <param name="privliage">level of priviliage of the executor. must be => 2.</param>
+        /// <param name="newActiveEmployee">the desired employee to become the active employee.</param>
         public static void ChangeActiveEmployee(int privliage, Employee newActiveEmployee)
         {
             if (privliage >= 2) _activeEmployee = newActiveEmployee;
             else MessageBox.Show("Privliage isnt high enought. Active Employee not changing...");
         }
 
+        /// <summary>removes active employee.</summary>
+        /// <output>sets the active employee to null</output>
+        /// <example>could be used for logging-out.</example>
         public static void RemoveActiveEmployee()
-            { _activeEmployee = null; }
+        {
+            _activeEmployee = null;
+        }
     }
 }
