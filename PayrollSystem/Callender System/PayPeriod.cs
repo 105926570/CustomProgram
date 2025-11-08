29ea944cb5770c954823bc772e6cbcd83747ef4f
Thomas Federico <h@THOMAS-PC>
2025-11-08 23:48:17 +1100

    formatting in PayPeriod.cs

--- PayrollSystem/Callender System/PayPeriod.cs
+++ PayrollSystem/Callender System/PayPeriod.cs
@@ -21,11 +21,14 @@ namespace PayrollSystem.CallenderSystem
             _startDate = _startDate.Date.AddDays(-daysSinceMonday);
             _endDate = _startDate.AddDays(7);
         }
+
         public PayPeriod(DateTime startDate)
         {
             _startDate = startDate;
             _endDate = startDate.AddDays(7);
         }
+
+        //this is problemattic, as endDate must ALLWAYS ve startDate + 7 days. --- possibly remove?
         public PayPeriod(DateTime startDate, DateTime endDate)
         {
             _startDate = startDate;
