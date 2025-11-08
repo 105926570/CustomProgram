db8401ea8d49d27e18a62e1b4ceb2bb120d1c9e2
Thomas Federico <105926570@student.swin.edu.au>
2025-11-08 15:36:21 +1100

    added Employee form

---
+++ PayrollSystem/Forms/Employee.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PayrollSystem.Forms
+{
+    partial class Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(24, 23);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(434, 67);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Welcome  User";
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 666);
+            this.Controls.Add(this.lblWelcome);
+            this.Name = "Employee";
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.Employee_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcome;
+    }
+}
---
+++ PayrollSystem/Forms/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayrollSystem.Forms
+{
+    public partial class Employee : Form
+    {
+        private string _welcomeName = "User";
+
+        /// <summary>
+        /// Initialise with the default name [User]
+        /// </summary>
+        public Employee()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initialise with a given name [User]
+        /// </summary>
+        /// <param name="name"></param>
+        public Employee(string name) : this() //Users name should be included when the menu is launched from the login.
+        {
+            _welcomeName = name;
+        }
+
+        // Upon load, change the welcome text to include the users name.
+        private void Employee_Load(object sender, EventArgs e)
+        {
+            lblWelcome.Text = $"Welcome {_welcomeName}";
+        }
+    }
+}
