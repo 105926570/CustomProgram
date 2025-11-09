2d98ca899bcc2ad223a8ef384734f0bc4659847e
Thomas Federico <105926570@student.swin.edu.au>
2025-11-09 19:32:19 +1100

    Added PayHistories and EmployeesOnPayroll Pseudocode.

--- PayrollSystem/Money/Payroll.cs
+++ PayrollSystem/Money/Payroll.cs
@@ -41,3 +41,43 @@ namespace PayrollSystem
         }
     }
 }
+
+
+
+
+
+
+/******************************************************************************
+
+PayHistories Pseuidocode:
+
+        public List<PayHistory> PayHistories
+        {
+            get {
+                //read the files of every user
+                //save the payhistories
+                //convert them to actual payHistories
+                //return them
+                return _payHistories;
+            }
+            set { _payHistories = value; }
+        }
+
+******************************************************************************/
+
+/******************************************************************************
+
+
+EmployeesOnPayroll Pseudocode:
+
+        public List<Employee> EmployeesOnPayroll
+        {
+            get {
+                // just set this equal to the Company.Employee since all of their employees areon the payroll anyway
+                // return it
+                return _employeesOnPayroll;
+            }
+            set { _employeesOnPayroll = value; }
+        }
+
+******************************************************************************/
