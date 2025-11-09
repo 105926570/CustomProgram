62c9ebc383847b778f4160d3c2aad88725fb3f70
Thomas Federico <105926570@student.swin.edu.au>
2025-11-09 17:17:01 +1100

    Added IdentifiableObjectClass.

---
+++ PayrollSystem/IdentifiableObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSystem
+{
+    public class IdentifiableObject
+    {
+        private int _id;
+
+        public IdentifiableObject() //default constructor
+        {
+            _id = 0; //should be application.generateuniqeid
+        }
+
+        public IdentifiableObject(int id) //to be used when loading something from a json for example
+        {
+            _id = id;
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
