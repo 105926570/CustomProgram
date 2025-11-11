8a6e208df1cf188dc38eb593051fc28be8e7a186
105926570 <105926570@student.swin.edu.au>
2025-11-11 22:43:16 +1100

    note

--- PayrollSystem/CallenderSystem/Shift.cs
+++ PayrollSystem/CallenderSystem/Shift.cs
@@ -8,6 +8,7 @@ namespace PayrollSystem
 {
     public class Shift : TimeFrame
     {
+        // these both should be here and not time frame, as they determine the final pay of shift. this is not needed for leave, or avaliability.
         public bool isWeekend { get { return StartDateTime.DayOfWeek == DayOfWeek.Saturday || StartDateTime.DayOfWeek == DayOfWeek.Sunday; } }
         public float HoursScheduled { get { return StartDateTime.CompareTo(EndDateTime); } }
     }
