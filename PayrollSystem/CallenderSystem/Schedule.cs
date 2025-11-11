281d7e548ac4b9010aba76d2b5b81fd1e9864745
105926570 <105926570@student.swin.edu.au>
2025-11-11 22:28:04 +1100

    Remade Schedule

---
+++ PayrollSystem/CallenderSystem/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PayrollSystem
+{
+    public class Schedule
+    {
+        public TimeFrame[] TimeFrames { get; set; }
+
+        /// <summary>Creates a schedule with a blank time frame </summary>
+        public Schedule() : this(new TimeFrame[] { }) { }
+        /// <summary> Creates a schedule with an array of given time frames </summary>
+        /// <param name="timeFrames"> The time frames witch will be added to the schedule. </param>
+        public Schedule(TimeFrame[] timeFrames) { TimeFrames = timeFrames; }
+    }
+}
