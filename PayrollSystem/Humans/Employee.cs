085648cf1fa1749102a267b3f7d2f212948e1157
105926570 <105926570@student.swin.edu.au>
2025-11-11 22:22:47 +1100

    Revert "Branch to merge date time and pay check into master."

--- PayrollSystem/Humans/Employee.cs
+++ PayrollSystem/Humans/Employee.cs
@@ -6,42 +6,55 @@ namespace PayrollSystem
 {
     public class Employee : User
     {
-
-/******************** PROPERTIES ********************/
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public EmployeeTaxInfo EmployeeTaxInfo { get; set; }
-        public Schedule Schedule { get; set; }
-        public PayHistory PayHistory { get; set; }
-        [JsonIgnore]
-        public string FullName { get { return FirstName + " " + LastName; } }
+        private string _firstName, _lastName;
+        private EmployeeTaxInfo _employeeTaxInfo;
+        //Department department;
+        private Schedule _schedule;
+        private PayHistory _payHistory;
+        private float _hourlyRate;
         private DateTime _birthDate; //to be inputted when creating employee
 
-        /****************************************************/
-
-        //Defaul Constructor
-        public Employee() : base()
+        public Employee() : base() //Constructor with default values
         {
-            FirstName = RandomFirstName();
-            LastName = RandomLastName();
-            EmployeeTaxInfo = new EmployeeTaxInfo();
-            PayHistory = new PayHistory();
-            Schedule = new Schedule();
+            _firstName = "Jhon";
+            _lastName = "Doe";
+            _employeeTaxInfo = new EmployeeTaxInfo();
+            _payHistory = new PayHistory();
+            _schedule = new Schedule();
         }
 
         public Employee(string firstName, string lastName, string Username, string Password) : base(Username, Password)
         {
-            FirstName = firstName;
-            LastName = lastName;
+            _firstName = firstName;
+            _lastName = lastName;
         }
 
-        public Employee(string firstName, string lastName, string Username, string Password, EmployeeTaxInfo taxInfo, Schedule schedule, PayHistory payHistory) : this(firstName, lastName, Username, Password)
+        // Read only Property - as in can only be changed by the class and not by a function.
+        // FullName should ALLWAYS reflect: $"{FirstName} {LastName}"
+        [JsonIgnore]
+        public string FullName
         {
-            EmployeeTaxInfo = taxInfo;
-            PayHistory = payHistory;
-            Schedule = schedule;
+            get { return _firstName + " " + _lastName; } //test if this updates every time its ran.
         }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public PayHistory PayHistory
+        {
+            get { return _payHistory; }
+            set { _payHistory = value; }
+        }
+
 
         // Maybe change to string so that its up to the caller to print it?
         public void DisplayEmployeeInfo()
