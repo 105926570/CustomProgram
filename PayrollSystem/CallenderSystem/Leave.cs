56e14ca94a9db408014ed886cdf7fafdc686563a
105926570 <105926570@student.swin.edu.au>
2025-11-11 22:51:13 +1100

    updated Leave class

--- PayrollSystem/CallenderSystem/Leave.cs
+++ PayrollSystem/CallenderSystem/Leave.cs
@@ -8,6 +8,8 @@ namespace PayrollSystem.CallenderSystem
 {
     public class Leave : TimeFrame
     {
-        public Leave() { }
+        public string Reason { get; set; }
+        public Leave() : this("General") { } //Default reason is General leave
+        public Leave(string reason_for_leave) { Reason = reason_for_leave; }
     }
 }
