93f4000871bc91cf4cd3ccafd1daa71eeb50af8d
105926570 <105926570@student.swin.edu.au>
2025-11-11 22:47:49 +1100

    created avaliability class

---
+++ PayrollSystem/CallenderSystem/Avaliability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSystem.CallenderSystem
+{
+    public class Avaliability : TimeFrame
+    {
+        public bool Avaliable { get; set; }
+
+        public Avaliability() : this(true) { } //by efaul avaliable
+        public Avaliability(bool avaliability) { Avaliable = avaliability; }
+    }
+}
