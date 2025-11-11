acd870fd2e21c18f6996721c321cfb9daa336998
Thomas Federico <105926570@student.swin.edu.au>
2025-11-11 20:51:50 +1100

    removed employees on payroll

--- PayrollSystem/Money/Payroll.cs
+++ PayrollSystem/Money/Payroll.cs
@@ -5,35 +5,15 @@ namespace PayrollSystem
 {
     public class Payroll
     {
-        private List<Employee> _employeesOnPayroll;
         private List<PayHistory> _payHistories;
 
 
 
-        public Payroll()
-        {
-            PayHistories = new List<PayHistory>() { };
-            EmployeesOnPayroll = new List<Employee> { };
-        }
+        public Payroll() : this(new List<PayHistory>()) { }
 
-        /// <summary>
-        /// This is just for generating a payroll. run this with company.Employees as the parameter, and it generates the companies payroll. Upoon the creation of an employee, their PayHistory should be created. an Employees payhistory must be consistently updated with schedule, shift, ect. 
-        /// </summary>
-        /// <param name="employees"></param>
-        public Payroll(List<Employee> employees) : this()
+        public Payroll(List<PayHistory> payHistories)
         {
-            EmployeesOnPayroll = employees;
-            foreach (Employee emp in employees)
-            {
-                PayHistories.Add(emp.PayHistory);
-            }
-        }
-
-        [JsonIgnore] //as EmployeesOnPayroll is the same as Company Departments Employees, and thus does not need to be saved twice.
-        public List<Employee> EmployeesOnPayroll
-        {
-            get { return _employeesOnPayroll; }
-            set { _employeesOnPayroll = value; }
+            PayHistories = payHistories;
         }
 
         public List<PayHistory> PayHistories
