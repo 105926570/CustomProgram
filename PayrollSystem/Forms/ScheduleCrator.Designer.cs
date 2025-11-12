1dbb0236419c47853d42cf8683b4964656ef1c34
105926570 <105926570@student.swin.edu.au>
2025-11-12 16:44:34 +1100

    *added Schedule Creator *added employee selector to schedule creator

---
+++ PayrollSystem/Forms/ScheduleCrator.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PayrollSystem
+{
+    partial class ScheduleCrator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbxEmployees = new System.Windows.Forms.ComboBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbxEmployees
+            // 
+            this.cmbxEmployees.FormattingEnabled = true;
+            this.cmbxEmployees.Location = new System.Drawing.Point(12, 12);
+            this.cmbxEmployees.Name = "cmbxEmployees";
+            this.cmbxEmployees.Size = new System.Drawing.Size(213, 33);
+            this.cmbxEmployees.TabIndex = 0;
+            this.cmbxEmployees.SelectedIndexChanged += new System.EventHandler(this.cmbxEmployees_SelectedIndexChanged);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(66, 97);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(70, 25);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "label1";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(66, 147);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(70, 25);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "label1";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(66, 215);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(70, 25);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "label1";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(83, 304);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(70, 25);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "label1";
+            // 
+            // ScheduleCrator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 662);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.cmbxEmployees);
+            this.Name = "ScheduleCrator";
+            this.Text = "ScheduleCrator";
+            this.Load += new System.EventHandler(this.ScheduleCrator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbxEmployees;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
---
+++ PayrollSystem/Forms/ScheduleCrator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static PayrollSystem.Program;
+
+namespace PayrollSystem
+{
+    public partial class ScheduleCrator : Form
+    {
+        private List<Employee> EmployeesFromActive = new List<Employee>() { };
+
+        public ScheduleCrator()
+        {
+            InitializeComponent();
+        }
+
+        private void ScheduleCrator_Load(object sender, EventArgs e)
+        {
+            cmbxEmployees.Items.Clear();
+            EmployeesFromActive = ActiveCompany.Employees;
+            foreach (Employee emp in EmployeesFromActive)
+            {
+                cmbxEmployees.Items.Add(emp.ID);
+            }
+        }
+
+        private void cmbxEmployees_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Employee emp = EmployeesFromActive[cmbxEmployees.SelectedIndex];
+            lblFirstName.Text = emp.FirstName;
+            lblLastName.Text = emp.LastName;
+            lblPassword.Text = emp.Password;
+            lblUsername.Text = emp.Username;
+        }
+    }
+}
